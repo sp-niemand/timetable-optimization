24741a62772fe6be9caf8157eaa8a8a5aa0682f2
Dmitri Cherepovski <codernumber1@gmail.com>
2016-01-03 20:25:28 +0200

    comment out temporary code

--- algorithm/dependent_timetable.py
+++ algorithm/dependent_timetable.py
@@ -1,15 +1,15 @@
-import algorithm.timetable as at
-import algorithm.dependency as ad
-
-def get_optimal_timetable(task_costs, dependency_graph):
-    """
-
-    :param numpy.matrix task_costs:
-    :param networkx.DiGraph dependency:
-    :return:
-    """
-    for (level, tasks) in ad.iterate_levels(dependency_graph):
-        # [0,3,4]
-        subtimetable = at.get_optimal_timetable(task_costs[:, tasks])
-        for processor_tasks in subtimetable:
-            processor_tasks
+# import algorithm.timetable as at
+# import algorithm.dependency as ad
+#
+# def get_optimal_timetable(task_costs, dependency_graph):
+#     """
+#
+#     :param numpy.matrix task_costs:
+#     :param networkx.DiGraph dependency:
+#     :return:
+#     """
+#     for (level, tasks) in ad.iterate_levels(dependency_graph):
+#         # [0,3,4]
+#         subtimetable = at.get_optimal_timetable(task_costs[:, tasks])
+#         for processor_tasks in subtimetable:
+#             processor_tasks
